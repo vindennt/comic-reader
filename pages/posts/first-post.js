cd62cea43dcea30d887a1da8eb231b9d78bdf237
dennistruong <truong.dennis00@gmail.com>
2023-02-28 22:55:59 -0800

    css components

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,25 +1,28 @@
 import Link from "next/link";
 import Image from "next/image";
 import Head from "next/head";
+import Script from "next/script";
+import Layout from "../../components/layout";
 
 export default function FirstPost() {
     return (
-    <>
+    <Layout>
     <Head>
         <title>
             First Post
         </title>
+
     </Head>
     <h1>First Post</h1>
     <h2>
         <Link href="/">Back to Home</Link>
-        <Image
+        {/* <Image
             src="/images/d.png"
             height={144}
             width={144}
             alt="D"
-        />
+        /> */}
     </h2>
-    </>
+    </Layout>
     );
 }
