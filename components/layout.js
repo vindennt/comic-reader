127f983091948a53e49f745e00f59decca99ff8c
dennistruong <truong.dennis00@gmail.com>
2023-03-05 22:30:00 -0800

    start chakra integration

--- components/layout.js
+++ components/layout.js
@@ -6,8 +6,8 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = 'Den'
-export const siteTitle = 'D'
+const name = 'Dennis Truong'
+export const siteTitle = 'vindennt'
 
 export default function Layout({ children, home }) {
   return (
@@ -39,7 +39,7 @@ export default function Layout({ children, home }) {
           <>
             <Image
               priority
-              src="/images/profile.jpg"
+              src="/images/d.png"
               className={utilStyles.borderCircle}
               height={144}
               width={144}
@@ -52,7 +52,7 @@ export default function Layout({ children, home }) {
             <Link href="/">
               <Image
                 priority
-                src="/images/profile.jpg"
+                src="/images/d.png"
                 className={utilStyles.borderCircle}
                 height={108}
                 width={108}
